80e8c5f8d5d9c98bf63900e5765c0a365ec3e47e
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2024-11-19 19:04:29 +0530

    Add FeaturedCourses component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center">Hello Pixels</h1>
       <HeroSection/>
+      <FeaturedCourses/>
     </main>
   );
 }
